94c555bb32237230b6dc3e7179e3525f2cb915c6
Martin Rusev <marto260503@abv.bg>
2023-07-31 15:26:16 +0300

    Add classes

---
+++ Classes/cats.js
@@ -0,0 +1,21 @@
+function cats (array) {
+
+    class Cat {
+        constructor (catName, age) {
+            this.name = catName;
+            this.age = age;
+        }
+
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    for (const line of array) {
+        let [name, age] = line.split(' ');
+
+        let cat = new Cat(name, age);
+        cat.meow();
+    }
+}
+cats(['Mellow 2', 'Tom 5'])
